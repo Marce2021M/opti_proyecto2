d1ee7bcd93786924130857531a43392aca46deed
marcelinoMSR <marcelino-sanchez2011@hotmail.com>
2024-05-02 17:14:36 -0600

    haciendo modificaciones del profe

--- minimize.py
+++ minimize.py
@@ -1,7 +1,13 @@
 from scipy.optimize import minimize
 from scipy.optimize import approx_fprime
 import numpy as np
-def minimize_func(func, x0, hfunc):
+from scipy.optimize import Bounds
+from scipy.optimize import LinearConstraint
+from scipy.optimize import NonlinearConstraint
+from scipy.optimize import BFGS
+from scipy.optimize import SR1
 
-    
-    return minimize(func, x0, method='SLSQP',constraints={'type':'eq', 'fun': hfunc}, options={'maxiter':400, 'ftol':1e-10})
+def minimize_func(func, x0, hfunc):
+    nonlinear_constraint =NonlinearConstraint(hfunc,0,0, jac='2-point',hess=BFGS())
+    #return minimize(func, x0, method='SLSQP',constraints={'type':'eq', 'fun': hfunc}, options={'maxiter':400, 'ftol':1e-10})
+    return minimize(func, x0, method='trust-constr',jac = '2-point',hess = SR1(),constraints=nonlinear_constraint, options={'maxiter':1600})
