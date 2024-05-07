b60ab0d9638cde5e2f29622547a7b1fd4819ee99
marcelinoMSR <marcelino-sanchez2011@hotmail.com>
2024-05-06 21:11:54 -0600

    falta arreglar a menos iteraciones como lo del profe

--- minimize.py
+++ minimize.py
@@ -9,5 +9,5 @@ from scipy.optimize import SR1
 
 def minimize_func(func, x0, hfunc):
     nonlinear_constraint =NonlinearConstraint(hfunc,0,0, jac='2-point',hess=BFGS())
-    #return minimize(func, x0, method='SLSQP',constraints={'type':'eq', 'fun': hfunc}, options={'maxiter':400, 'ftol':1e-10})
-    return minimize(func, x0, method='trust-constr',jac = '2-point',hess = SR1(),constraints=nonlinear_constraint, options={'maxiter':1600})
+    #return minimize(func, x0, method='SLSQP',constraints={'type':'eq', 'fun': hfunc})
+    return minimize(func, x0, method='trust-constr',jac = '2-point',hess = SR1(),constraints=nonlinear_constraint,tol=1e-6)
