e87f5651e67ed55a6ed1d995b7cef1171688bb29
marcelinoMSR <marcelino-sanchez2011@hotmail.com>
2024-05-13 04:01:39 -0600

    terminada mi parte

--- Marce/minimize.py
+++ Marce/minimize.py
@@ -1,13 +1,54 @@
+# --------------------------------------------------------------
+# Librerias que utiliza nuestro archivo
+# --------------------------------------------------------------
+
 from scipy.optimize import minimize
 from scipy.optimize import approx_fprime
 import numpy as np
-from scipy.optimize import Bounds
-from scipy.optimize import LinearConstraint
 from scipy.optimize import NonlinearConstraint
 from scipy.optimize import BFGS
-from scipy.optimize import SR1
+import time
+import pandas as pd
+import plotly.graph_objects as go
+
+# --------------------------------------------------------------
+# Definimos las funciones que vamos a utilizar
+# --------------------------------------------------------------
 
 def minimize_func(func, x0, hfunc):
     nonlinear_constraint =NonlinearConstraint(hfunc,0,0, jac='2-point',hess=BFGS())
-    #return minimize(func, x0, method='SLSQP',constraints={'type':'eq', 'fun': hfunc})
-    return minimize(func, x0, method='trust-constr',jac = '2-point',hess = SR1(),constraints=nonlinear_constraint,tol=1e-6)
+    return minimize(func, x0, method='SLSQP',constraints={'type':'eq', 'fun': hfunc})
+
+def f_electron(x, puntoInicial = np.array([1,0,0])):
+    # x es un vector columna de dimensi ́on 3 ∗ np. (np.array)
+    # El punto #, k, xk que quiere colocarse en la esfera,
+    # tiene coordenadas (x3∗k, x3k+1, x3∗k+2).
+    # Return:
+    # f−x valor de la funci ́on a minimizar.
+    nproton = len(x)//3 + 1
+    x = np.array(x)
+    puntoInicial = np.array(puntoInicial)
+    x = np.concatenate((puntoInicial, x))
+    x = np.reshape(x, (nproton, 3))
+    resultado = 0
+    for i in range(nproton):
+        for j in range(nproton):
+            if i < j:
+                resultado += 1/np.linalg.norm(x[i]-x[j])
+                
+    return resultado
+
+def h_esfera(x):
+    # versi ́on para usar en programaci ́on cuadr ́atica sucesiva
+    # x es un vector columna de dimensi ́on 3 ∗ np.
+    # El punto #, k, xk que quiere colocarse en la esfera,
+    # tiene coordenadas (x[3 ∗ k], x[3k + 1], x[3 ∗ k + 2]).
+    # Return h−x .
+    # h−x es el vector de dimensi ́on np que indica
+    # si el punto xk est ́a cerca o no de la esfera.
+    # h−x[k] = x[3 ∗ k] ∗ ∗(2) + x[3k + 1] ∗ ∗(2) + x[3 ∗ k + 2] ∗ ∗(2) − 1
+    #
+    x = np.array(x)
+    nproton = len(x)//3 
+    x = np.reshape(x, (nproton, 3))
+    return np.linalg.norm(x, axis = 1)**2 - 1
--- Marce/pcs_minimize.py
+++ Marce/pcs_minimize.py
@@ -1,27 +1,66 @@
----
-title: Resultados de la optimización de puntos en la esfera (SCIPY)
-lang: es
-date: today
-format:
-  html:
-    page-layout: full
-    embed-resources: true
-    code-fold: true
-    toc: true
-    toc-depth: 3
-nocite: |
-  @*
----
+# Integrantes del equipo:
 
-# Problema de optimización
+# --------------------------------------------------------------
+# Librerias que utiliza nuestro archivo
+# --------------------------------------------------------------
 
-$\sum_{k=0}^{np}\sum_{k<j} \frac{1}{\lVert x_{k}-x_{j}\rVert ^{-1}}$
+from scipy.optimize import minimize
+from scipy.optimize import approx_fprime
+import numpy as np
+from scipy.optimize import Bounds
+from scipy.optimize import LinearConstraint
+from scipy.optimize import NonlinearConstraint
+from scipy.optimize import BFGS
+from scipy.optimize import SR1
+import time
+import pandas as pd
+import plotly.graph_objects as go
 
-# Resolución del problema
+# --------------------------------------------------------------
+# Definimos las funciones que vamos a utilizar
+# --------------------------------------------------------------
 
-```{python}
-# Usamos el módulo scipy.optimize para minimizar la función f_electron
-from minimize import *
+def minimize_func(func, x0, hfunc):
+    nonlinear_constraint =NonlinearConstraint(hfunc,0,0, jac='2-point',hess=BFGS())
+    return minimize(func, x0, method='SLSQP',constraints={'type':'eq', 'fun': hfunc})
+
+def f_electron(x, puntoInicial = np.array([1,0,0])):
+    # x es un vector columna de dimensi ́on 3 ∗ np. (np.array)
+    # El punto #, k, xk que quiere colocarse en la esfera,
+    # tiene coordenadas (x3∗k, x3k+1, x3∗k+2).
+    # Return:
+    # f−x valor de la funci ́on a minimizar.
+    nproton = len(x)//3 + 1
+    x = np.array(x)
+    puntoInicial = np.array(puntoInicial)
+    x = np.concatenate((puntoInicial, x))
+    x = np.reshape(x, (nproton, 3))
+    resultado = 0
+    for i in range(nproton):
+        for j in range(nproton):
+            if i < j:
+                resultado += 1/np.linalg.norm(x[i]-x[j])
+                
+    return resultado
+
+def h_esfera(x):
+    # versi ́on para usar en programaci ́on cuadr ́atica sucesiva
+    # x es un vector columna de dimensi ́on 3 ∗ np.
+    # El punto #, k, xk que quiere colocarse en la esfera,
+    # tiene coordenadas (x[3 ∗ k], x[3k + 1], x[3 ∗ k + 2]).
+    # Return h−x .
+    # h−x es el vector de dimensi ́on np que indica
+    # si el punto xk est ́a cerca o no de la esfera.
+    # h−x[k] = x[3 ∗ k] ∗ ∗(2) + x[3k + 1] ∗ ∗(2) + x[3 ∗ k + 2] ∗ ∗(2) − 1
+    #
+    x = np.array(x)
+    nproton = len(x)//3 
+    x = np.reshape(x, (nproton, 3))
+    return np.linalg.norm(x, axis = 1)**2 - 1
+
+# --------------------------------------------------------------
+# Código para resolver el problema de optimización
+# --------------------------------------------------------------
 
 # Definimos el número de puntos aleatorios que vamos a organizar
 # en la esfera menos 1 porque en la optimización se fija un punto de los np +1
@@ -52,11 +91,10 @@ grad_lagrangiana = np.concatenate((grad_lagrangiana,h_esfera(resultado.x) ))
 # Calculamos la norma del gradiente de la función lagrangiana
 norm_grad_lagrangiana = np.linalg.norm(grad_lagrangiana)
 
-```
+# --------------------------------------------------------------
+# Presentación de resultados
+# --------------------------------------------------------------
 
-# Tabla de resultados
-
-```{python}
 # Creación de la tabla
 
 data = {
@@ -69,14 +107,8 @@ data = {
 
 print(pd.DataFrame(data)) # Resultados de la tabla
 
-```
 
-
-# Gráfica de los resultados
-
-```{python}
-import plotly.graph_objects as go
-import numpy as np
+# Creación de gráfica
 
 # Supongamos que esta es la salida de tu optimización, por ejemplo
 points = resultado.x.reshape(-1, 3)
@@ -120,8 +152,3 @@ fig.update_layout(
 
 # Mostrar el gráfico
 fig.show()
-
-```
-
-
-
